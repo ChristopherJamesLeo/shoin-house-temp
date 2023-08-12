121eb373880bbd8e19c1ac20525dc83feb9f3756
ChristopherJamesLeo <aungaungoo.hotmail@gmail.com>
2023-08-12 16:51:01 +0630

    12-8-2023

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log("hello shoin house");
+// start jquery area
+$(document).ready(function(){
+    $("#banner_coursel").owlCarousel({
+        items : 1,
+        loop : true,
+        dots : false,
+        // autoplay : true
+    });
+
+    $(".product_edit_btn").click(function(){
+        // console.log($(this));
+        $(this).toggleClass("active");
+        // console.log($(this).parent()); // result = product_main_container 
+        // console.log($(this).parent().find(".product_name_group")); // get product_name_group class
+        $(this).parent().find(".product_name_group").toggleClass("active");
+        // console.log($(this).parent().find(".product_edit_form")); 
+        $(this).parent().find(".product_edit_form").toggleClass("active");
+    })
+
+})
+// end jquery area
